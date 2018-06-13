2497bbe5eb3b59e1cc25cfcc83c212ee47d0301d
Mourya Raj <Mourya Raj@Mourya-Laptop>
2018-06-13 17:15:24 -0500

    Decimal to Binary

---
+++ Assignment/src/com/Sun/Eng/DecimaltoBinary.java
@@ -0,0 +1,9 @@
+package com.Sun.Eng;
+
+public class DecimaltoBinary {
+
+	public static void main(String args[]) {
+		Integer Value=new Integer(10);
+		System.out.println("Binary value is "+ Integer.toString(Value,2));
+	}
+}
